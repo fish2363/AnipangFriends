eb0379698a1e0e3fa253f3214120660eb5079be7
dongho <jijiji0809160@gmail.com>
2025-11-19 20:21:53 +0900

    feat : map

---
+++ AnipangProject/Assets/Work/CDH/Code/Maps/MapManager.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Work.CDH.Code.Maps
+{
+    public class MapManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject mapPrefab;
+
+        private List<Map> currentMaps;
+
+        // 맵 처음 만들 때 초기화
+        public void Initialize()
+        {
+            
+        }
+    }
+}
